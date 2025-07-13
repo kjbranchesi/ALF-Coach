a0c403942654b46b9bb0d3dc5fdac9ee5dcb0774
kjbranchesi <kjbranchesi@gmail.com>
2025-07-13 13:59:58 +0000

    tailwind issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add the tailwindcss() plugin here. This is the crucial missing piece.
   plugins: [react(), tailwindcss()],
 });
