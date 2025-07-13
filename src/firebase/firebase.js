f3b89f3d055ec025cc1fd63fd562381b8903180e
kjbranchesi <kjbranchesi@gmail.com>
2025-07-13 22:12:17 +0000

    black and white

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,15 +5,9 @@ import { getFirestore } from 'firebase/firestore';
 // This reads the secure environment variable we set up
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG || '{}');
 
-console.log('Parsed firebaseConfig:', firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-console.log('Firebase app initialized:', app);
-
 // Initialize and export Firebase services
 export const auth = getAuth(app);
-console.log('Firebase auth initialized:', auth);
 export const db = getFirestore(app);
-console.log('Firebase db initialized:', db);
