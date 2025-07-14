99b280b061f90ba0dfc3639aa3b7756528dee9ee
kjbranchesi <kjbranchesi@gmail.com>
2025-07-14 01:41:04 +0000

    fix: Explicitly configure PostCSS for production build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Import the plugin
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Explicitly telling Vite to use PostCSS for all CSS files
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
 });
